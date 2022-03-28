784b10f5e3352b64738617021b30c121aeeafbe5
Kichu244 <94222281+Kichu244@users.noreply.github.com>
2022-03-28 11:31:04 +0530

    Git link Updated

--- plugins/xlove.js
+++ plugins/xlove.js
@@ -7,21 +7,18 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/ZzqO1v5.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/ygQuzYd.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot Name: Botmwol*
 
 *Creator number : wa.me/918547487332?text=Hi%20Kichu%20Mon.%20*
 
  *To check update .update   To update Bot .update now*
-
- *Bot making video : https://youtu.be/Tb1B-rS52uo*
      
- *Instagram id: https://www.instagram.com/invites/contact/?i=1iehqmafm9pdc&utm_content=mktgo1n*
+ *𝐈𝐧𝐭𝐚𝐠𝐫𝐚𝐦 𝐈𝐝: https://www.instagram.com/invites/contact/?i=1iehqmafm9pdc&utm_content=mktgo1n*
 
- *Yt_PASSWORD: _________(password is allset)*
 
- *githublink : https://github.com/Kichu244/BM-AMLV1*
+ 𝐆𝐢𝐭𝐇𝐮𝐛 𝐋𝐢𝐧𝐤 𝐍𝐨𝐭 𝐀𝐯𝐚𝐢𝐥𝐚𝐛𝐥𝐞.
 
  *Botmwol is a chatbot.Enjoy with BotMwol🥰*
 `}) 
