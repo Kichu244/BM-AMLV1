8d1a75f3426bf553d4d48a865bcc9be30f85e46d
Kichu244 <94222281+Kichu244@users.noreply.github.com>
2022-04-01 10:05:46 +0530

    Updated

--- plugins/xlove.js
+++ plugins/xlove.js
@@ -10,10 +10,10 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the
     var skl = await axios.get("https://i.imgur.com/ygQuzYd.jpeg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bot Name: BOTMWOL*
-*=›*𝐎𝐰𝐧𝐞𝐫||𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫||𝐂𝐫𝐞𝐚𝐭𝐨𝐫
-    𝑪𝒐𝒏𝒕𝒂𝒄𝒕:- https://wa.me/918547487332
+✈︎ 𝐎𝐰𝐧𝐞𝐫||𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫||𝐂𝐫𝐞𝐚𝐭𝐨𝐫
+ 𝑪𝒐𝒏𝒕𝒂𝒄𝒕:- https://wa.me/918547487332
  
-*=›* 𝑮𝑰𝑻𝑯𝑼𝑩 𝑳𝑰𝑵𝑲 𝑰𝑺 𝑵𝑶𝑻 𝑨𝑽𝑨𝑰𝑳𝑨𝑩𝑳𝑬
+✈︎ 𝑮𝑰𝑻𝑯𝑼𝑩 𝑳𝑰𝑵𝑲 𝑰𝑺 𝑵𝑶𝑻 𝑨𝑽𝑨𝑰𝑳𝑨𝑩𝑳𝑬
 
      𝑰 𝒉𝒐𝒑𝒆 𝒀𝒐𝒖 𝑬𝒏𝒋𝒐𝒚𝒆𝒅 𝑾𝒊𝒕𝒉 𝑩𝒐𝒕𝒎𝒘𝒐𝒍
                𝑻𝒉𝒂𝒏𝒌 𝒀𝒐𝒖
