028cde80cd983af16d3dc107ed597a9398ff42e0
Kichu244 <94222281+Kichu244@users.noreply.github.com>
2022-04-07 12:24:04 +0530

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -2,6 +2,6 @@ const New = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const DEL_DESC = "Deletes replied message! (owner command)"
 
-New.addCommand({pattern: 'dlt', fromMe: true, desc: DEL_DESC}, (async (multidevice, match) => {
-await multidevice.client.deleteMessage(multidevice.jid, {id: multidevice.reply_message.id, remoteJid: multidevice.reply_message.jid, fromMe: true})
+New.addCommand({pattern: 'dlt', fromMe: false, desc: DEL_DESC}, (async (multidevice, match) => {
+await multidevice.client.deleteMessage(multidevice.jid, {id: multidevice.reply_message.id, remoteJid: multidevice.reply_message.jid, fromMe: false})
 }));
