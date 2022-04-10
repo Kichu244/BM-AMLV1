78c38f9e7fa2a2a89a9993ae06528db0d97f9e56
Kichu244 <94222281+Kichu244@users.noreply.github.com>
2022-04-10 21:25:21 +0530

    Add files via upload

---
+++ plugins/filters2.js
@@ -0,0 +1,41 @@
+const asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const Config = require('../config');
+const Language = require('../language');
+if (Config.WORKTYPE == 'private') {
+
+kichu.addCommand({pattern: 'stiker ?(.*)', fromMe: false, deleteCommand: true, desc: Lang.MSG_DESC}, (async (message, match) => {
+    var r_text = new Array ();
+    r_text[1] = "Invalid Command, Use  *.sticker*  To Convert Image or Video to Sticker.";
+    var i = Math.floor(2*Math.random())
+    await message.client.sendMessage(
+        message.jid,(r_text[i]), MessageType.text);
+    }));
+
+Kichu.addCommand({pattern: 'image (.*)', fromMe: true, deleteCommand: true, desc: Lang.MSG_DESC}, (async (message, match) => {
+        var r_text = new Array ();
+        r_text[1] = "Invalid Command. Use *.photo* To Convert Sticker To Image. Or  Use  *,img (name)* To Get Image From Google";
+        var i = Math.floor(2*Math.random())
+        await message.client.sendMessage(
+            message.jid,(r_text[i]), MessageType.text);
+        }));    
+    }
+
+if (Config.WORKTYPE == 'public') {
+
+Kichu.addCommand({pattern: 'stiker (.*)', fromMe: true, deleteCommand: true, desc: Lang.MSG_DESC}, (async (message, match) => {
+    var r_text = new Array ();
+    r_text[1] = "Invalid Command, Use  *.sticker*  To Convert Image or Video to Sticker.";
+    var i = Math.floor(2*Math.random())
+    await message.client.sendMessage(
+        message.jid,(r_text[i]), MessageType.text);
+    }));
+
+Kichu.addCommand({pattern: 'image (.*)', fromMe: true, deleteCommand: true, desc: Lang.MSG_DESC}, (async (message, match) => {
+        var r_text = new Array ();
+        r_text[1] = "Invalid Command. Use *.photo* To Convert Sticker To Image. Or  Use  *,img (name)* To Get Image From Google";
+        var i = Math.floor(2*Math.random())
+        await message.client.sendMessage(
+            message.jid,(r_text[i]), MessageType.text);
+        }));    
+    }
